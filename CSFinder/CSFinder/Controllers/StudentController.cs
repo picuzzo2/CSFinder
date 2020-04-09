756adeaff05cb524eb865ac9d65283535d80c413
Jakkrit Boonnet <dekthefest_29@hotmail.com>
2020-04-10 00:03:13 +0700

    link name and status

--- CSFinder/CSFinder/Controllers/StudentController.cs
+++ CSFinder/CSFinder/Controllers/StudentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CSFinder.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace CSFinder.Controllers
 {
@@ -26,7 +27,24 @@ namespace CSFinder.Controllers
         [Route("Home")]
         public IActionResult Home()
         {
-            return View();
+            string userID = HttpContext.Session.GetString("UserID");
+            string IDType = HttpContext.Session.GetString("IDType");
+            if (IDType != null)
+            {
+
+                if (IDType == "Student")
+                {
+                    var obj = db.Students.Where(a => a.ID.Equals(HttpContext.Session.GetString("UserID"))).FirstOrDefault();
+                    ViewBag.Name = obj.Name.ToString();
+                    ViewBag.Status = obj.Status.ToString();
+                }                
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
         }
         [Route("Profile")]
         public IActionResult Profile()
